48c2623469f4121dbd67487b8723f186dbf17a42
jason.regina <jason.regina@noaa.gov>
2026-06-12 18:38:18 +0000

    find potential storm basin combinations

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 """Main entry to data pipeline."""
 from pathlib import Path
 import logging
+from itertools import count
+
+import pandas as pd
+
 from src.cyclone_streamflow.configuration import load_configuration, DataType
 from src.cyclone_streamflow.pipelines import download_all_data, process_all_data
 
@@ -25,8 +29,30 @@ def main(
     configuration = load_configuration(configuration_filepath)
     sources = download_all_data(configuration)
     data = process_all_data(sources, directory=configuration.data_directory)
-    for k, v in data.items():
-        print(k, v.head(1))
+
+    # Match basins to storm tracks
+    number_of_gages = len(data[DataType.GAGES_II]["GAGE_ID"])
+    counter = count(1)
+    dfs = []
+    for index, gage_id, boundary in data[DataType.GAGES_II][["GAGE_ID", "geometry"]].itertuples():
+        # Report
+        logging.info("%s - %d / %d", gage_id, next(counter), number_of_gages)
+
+        # Clip storm tracks to basin boundary
+        subset = data[DataType.IBTRACS].clip(boundary.buffer(400.0))
+
+        # Check for storms
+        if subset.empty:
+            logging.info("No storms found")
+            continue
+
+        # Save storms
+        subset["GAGE_ID"] = gage_id
+        dfs.append(subset)
+
+    # Merge
+    result = pd.concat(dfs, ignore_index=True).sort_values(["GAGE_ID", "time"])
+    result.to_csv("test_result.csv")
 
 if __name__ == "__main__":
     main()
